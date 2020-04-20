e986d5afde4c242afe44e92b2fd93e2a341fe087
Ahmad <ahmadhafez@users.noreply.github.com>
2020-04-21 00:42:37 +1000

    Add files via upload

---
+++ TestContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Test_Framework.Models;
+
+namespace Test_Framework
+{
+    public class TestContext : DbContext
+    {
+
+        public TestContext(DbContextOptions<TestContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApiModel> Apis { get; set; }
+        public DbSet<TestModel> Tests { get; set; }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    base.OnConfiguring(optionsBuilder);
+        //}
+
+    }
+}
